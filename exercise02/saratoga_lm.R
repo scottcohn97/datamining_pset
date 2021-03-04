dedf4f2a34c36015f0d371c5d119e9c4d301300d
scottkc97 <scott97kc@gmail.com>
2021-03-04 09:36:16 -0600

    add q3

---
+++ exercise02/saratoga_lm.R
@@ -0,0 +1,46 @@
+## Script to beat for Q2 on Ex02
+
+library(tidyverse)
+library(ggplot2)
+library(modelr)
+library(rsample)
+library(mosaic)
+data(SaratogaHouses)
+
+glimpse(SaratogaHouses)
+
+####
+# Compare out-of-sample predictive performance
+####
+
+# Split into training and testing sets
+saratoga_split = initial_split(SaratogaHouses, prop = 0.8)
+saratoga_train = training(saratoga_split)
+saratoga_test = testing(saratoga_split)
+
+# Fit to the training data
+# Sometimes it's easier to name the variables we want to leave out
+# The command below yields exactly the same model.
+# the dot (.) means "all variables not named"
+# the minus (-) means "exclude this variable"
+lm1 = lm(price ~ lotSize + bedrooms + bathrooms, data=saratoga_train)
+lm2 = lm(price ~ . - pctCollege - sewer - waterfront - landValue - newConstruction, data=saratoga_train)
+lm3 = lm(price ~ (. - pctCollege - sewer - waterfront - landValue - newConstruction)^2, data=saratoga_train)
+
+coef(lm1) %>% round(0)
+coef(lm2) %>% round(0)
+coef(lm3) %>% round(0)
+
+# Predictions out of sample
+# Root mean squared error
+rmse(lm1, saratoga_test)
+rmse(lm2, saratoga_test)
+rmse(lm3, saratoga_test)
+
+# Can you hand-build a model that improves on all three?
+# Remember feature engineering, and remember not just to rely on a single train/test split
+
+
+# rmse1 = 79658.78
+# rmse2 = 67763.56
+# rmse3 = 67763.56
